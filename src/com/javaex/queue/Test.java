68f19e9a46947e0d7c5a145b58b7c3ac2816dc98
송성빈 <aktm1004@naver.com>
2022-05-30 21:37:31 +0900

    Queue

---
+++ src/com/javaex/queue/Test.java
@@ -0,0 +1,45 @@
+package com.javaex.queue;
+
+import java.util.Comparator;
+
+public class Test {
+	public static void main(String[] args) {
+			
+			ArrayQueue<Student> q = new ArrayQueue<>();
+	 
+			q.offer(new Student("어렵네", 92));
+			q.offer(new Student("큐라는것", 72));
+			q.offer(new Student("아오씨", 98));
+			q.offer(new Student("머리아파", 51));
+			
+			q.sort(customComp);	// Comparator을 넘겨준다. 
+			
+			for(Object a : q.toArray()) {
+				System.out.println(a);
+			}
+		}
+		
+		// 사용자 설정 comparator(비교기)
+		static Comparator<Student> customComp = new Comparator<Student>() {
+			@Override
+			public int compare(Student o1, Student o2) {
+				return o2.score - o1.score;
+			}
+		};
+		
+	}
+	 
+	class Student {
+		String name;
+		int score;
+		
+		Student(String name, int score){
+			this.name = name;
+			this.score = score;
+		}
+		
+		public String toString() {
+			return "이름 : " + name + "\t성적 : " + score;
+		}
+
+}
